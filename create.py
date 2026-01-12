7e5fa00c33ef7659b34bf529a2f10c84688f5844
dacent53 <gneo0194@gmail.com>
2026-01-12 19:53:42 +0300

    Update create.py

--- create.py
+++ create.py
@@ -5,8 +5,8 @@
 TikTok accounts maker created users algorithms x-ladon x-argus x-gorgon last version
 
 ## last version 43.1.15
-**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
-## telegram: @alethino
+**Contact for api :** [https://t.me/dacent53](https://t.me/dacent53)
+## telegram: @dacent53
   
 ```json
 [
@@ -152,5 +152,5 @@ TikTok accounts maker created users algorithms x-ladon x-argus x-gorgon last ver
     }
 ]
 ```
-**Contact for api :** [https://t.me/alethino](https://t.me/alethino)
-## telegram: @alethino
+**Contact for api :** [https://t.me/dacent53](https://t.me/dacent53)
+## telegram: @dacent53
